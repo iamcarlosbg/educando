e3483b5d07f1b9579f76df13808ba3004d3067f7
iamcarlosbg <vk_Carlos@msn.com>
2026-01-31 15:59:49 +0100

    Modify button click action in comun.js
    Updated button action to redirect to a new URL.

--- js/comun.js
+++ js/comun.js
@@ -1,20 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // Crear botón
   const boton = document.createElement("button");
   boton.className = "btn-inicio";
   boton.textContent = "⬅ Inicio";
 
-  // Acción del botón
   boton.addEventListener("click", () => {
     if (history.length > 1) {
       history.back();
     } else {
-      window.location.href = "../index.html";
+      window.location.href = "/educando/";
     }
   });
 
-  // Añadir al body
   document.body.appendChild(boton);
 
 });
